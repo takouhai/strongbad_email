82aa7793310127f87c2f0ac99e8dc48eaf8f6ac9
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2023-11-12 14:54:29 -0500

    fix: unwrap data so i can access the struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use serde::Deserialize;
 use std::fs;
 use toml;
@@ -15,8 +17,13 @@ struct Emails {
 
 fn main() {
     println!("Preeeeow, world.");
-    let toml_file = fs::read_to_string("src/sbemails.toml").expect("failed to read sbemails.toml");
-    let sbemails: Sbemails = toml::from_str(&toml_file).expect("failed to deserialize sbemails.toml");
-    println!("{:#?}", sbemails);
+    let toml_file = fs::read_to_string("src/sbemails.toml")
+        .expect("failed to read sbemails.toml");
+    let sbemails: Sbemails = toml::from_str(&toml_file)
+        .expect("failed to deserialize sbemails.toml");
+    let mut random_number = thread_rng();
+    let sbemail = sbemails.emails.choose(&mut random_number).unwrap();
+    println!("sbemail number: {:?}", sbemail.number);
+    println!("sbemail title: {:?}", sbemail.title);
 }
 
