4864f6fb19a8848e446b000fc3f7071018909879
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2023-11-12 14:03:02 -0500

    feat: add sbemails and print raw deserialization ...to console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use serde::Deserialize;
+use std::fs;
+use toml;
+
+#[derive(Debug, Deserialize)]
+struct Sbemails {
+    emails: Vec<Emails>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Emails {
+    number: u8,
+    title: String,
+}
+
 fn main() {
     println!("Preeeeow, world.");
+    let toml_file = fs::read_to_string("src/sbemails.toml").expect("failed to read sbemails.toml");
+    let sbemails: Sbemails = toml::from_str(&toml_file).expect("failed to deserialize sbemails.toml");
+    println!("{:#?}", sbemails);
 }
+
