a176e62ef4bbc5f827141aa608d7c544c283cb53
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2023-11-12 19:29:59 -0500

    fix: print all lines correctly and set title

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,15 @@ struct Emails {
 }
 
 fn main() {
-    println!("Preeeeow, world.");
     let the_paper = include_str!("sbemails.toml");
     let sbemails: Sbemails = toml::from_str(the_paper)
         .expect("failed to deserialize sbemails.toml");
     let mut random_number = thread_rng();
     let sbemail = sbemails.emails.choose(&mut random_number).unwrap();
-    println!("sbemail number: {:?}", sbemail.number);
-    println!("sbemail title: {:?}", sbemail.title);
-    println!("sbemail content: {:?}", sbemail.content);
+    println!("sbemail #{}: {}", sbemail.number, sbemail.title);
+    let sbemail_content = sbemail.content.split("\n");
+    for part in sbemail_content {
+        println!("{}", part);
+    }
 }
 
